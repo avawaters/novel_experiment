7cd47f909e59a5201f2ee86489a81de53fb7ff60
LilyYan2001 <lyan@stuy.edu>
2022-04-14 14:31:51 -0400

    updated stimuli

--- stimuli/version_X.js
+++ stimuli/version_X.js
@@ -1,56 +1,50 @@
 var version_x = [
  {
-   "scenario": "Alice and Olivia are lifelong friends. They have done everything together, from having almost daily playdates in first grade to being starters on their high school varsity basketball team. One day, Olivia gets her hair cut by a barber who gives her bangs. Olivia then takes a selfie and sends it to Alice, saying, 'hey bestie, checkout my new hairstyle!' Recently, Alice is starting to feel that her identity is too closely coupled to Olivia's. She has been trying to create her own identity by changing the way she looks, which is why she recently got bangs herself. However, Olivia, who does not seem to be picking up on this attempt at individuality, followed Alice's lead and also got bangs. Alice is annoyed that Olivia is copying her hairstyle. Even though she doesn't like the way it looks, Alice replies, 'omg I love it!!!'",
+   "scenario": "Alice and Olivia are lifelong friends. They have done everything together, from having playdates most days in first grade to being starters on their high school varsity basketball team. One day, Olivia gets her hair cut by a barber who gives her bangs. Olivia then takes a selfie and sends it to Alice, saying, 'hey bestie, checkout my new hairstyle!' Recently, Alice has started to feel that her identity is too closely coupled to Olivia's. She has been trying to create her own identity by changing the way she looks, which is why she recently got bangs herself. However, Olivia, who did not seem to pick up on this attempt at individuality, followed Alice's lead and also got bangs. Alice is annoyed that Olivia is copying her hairstyle. Even though she doesn't like the way it looks, Alice replies, 'omg I love it!!!'",
    "level": "c",
    "id": "1c",
    "mc_question": "Do you think that Olivia would have interpreted Alice's text as sincere or sarcastic?"
  },
  {
-   "scenario": "Catherine just got back from the gym after hitting a PR (personal record). She receives a text message and sees how it is from her partner, Jane. 'How'd the gym go?' it reads. Catherine responds, 'I'm feeling so pumped up I feel like I could lift a cow.'",
+   "scenario": "Catherine just got back from the gym after unexpectedly hitting a PR (personal record). She has been suffering from a back injury and is just starting her recovery phase, so she is super excited. Catherine's partner Jane knows how Catherine has been suffering from a back injury and is concerned that it might be too early for Catherine to return to the gym. Jane sends a text to Catherine. 'How'd the gym go? Is everything okay?' it reads. Catherine responds, 'I'm feeling so pumped up today I feel like I could lift a cow.'",
    "level": "a",
    "id": "2a",
    "mc_question": "Do you think Jane would interpret Catherine's message as sincere or sarcastic?"
  },
  {
-   "scenario": "Jack and Anastasia grew up in the same neighborhood and were best friends. Anastasia is about to turn 20, so Jack decides to surprise Anastasia by baking Anastasia a cake for her birthday. The thing about Jack, however, is that he is a terrible cook and has never cooked anything in his entire life. It thus is no surprise that Jack accidentally confuses salt with sugar without even realizing it. Putting salt in the cake, Jack thinks he's following the recipe and adding the sugar as required. After Anastasia's birthday party, Jack gives the gift-wrapped cake to Anastasia. The next morning, Anastasia cuts the cake and starts to eat a slice. Her stomach is very much ready for a soft piece of cake that's sweet and creamy, signifying a perfect start of her 20-year-old life. 'Ewww!' However, it isn't long before Anastasia spits the cake out and grabs water to clean up her throat. 'Why is this so salty????' Picking up her phone, Anastasia sends a text to Jack saying 'The cake is perfect, aren't you so sweet!'",
+   "scenario": "Jack and Anastasia grew up in the same neighborhood and are currently still best friends. Anastasia is about to turn 20, so Jack decides to surprise her by baking her a cake for her birthday. Jack, however, is a terrible cook and has never baked anything in his life. As one would expect given his level of skill, Jack accidentally confuses salt with sugar without realizing it. After Anastasia's birthday party, Jack gives the gift-wrapped cake to Anastasia. The next morning, Anastasia cuts the cake and starts to eat a slice. Her stomach is ready for a soft piece of sweet, creamy cake, signifying a perfect start to her 20-year-old life... 'Ewww!' It isn't long before Anastasia spits the cake out and grabs water to clean up her throat, muttering to herself, 'Why is this so salty?' Picking up her phone, Anastasia sends a text to Jack saying, 'The cake is perfect, aren't you so sweet!'",
    "level": "c",
    "id": "3c",
    "mc_question": "Do you think Jack would interpret Anastasia's message as sincere or sarcastic?"
  },
  {
-   "scenario": "Margaret isn't exactly a dog person. She and her new coworker, Julia, are chatting via iMessage, when Julia sends Margaret a photo of her bulldog. Margaret responds, 'Your dog is so cute!'",
+   "scenario": "Margaret isn't exactly a dog person, and she has a particularly strong aversion to bulldogs. She and her new coworker, Julia, are chatting via iMessage when Julia sends Margaret a photo of her puppy, which is a bulldog. Most of Julia's friends think her dog is very cute. Margaret responds, 'Your dog is so scary!'",
    "level": "a",
    "id": "4a",
    "mc_question": "Do you think Julia would interpret Margaret's questions as sincere or sarcastic?"
  },
  {
-   "scenario": "Noah is a college student. Professor Oliver teaches an Intro to Computer Science class, one that Noah enrolled in. Noah was never a computer science person. He takes the course simply because of a mandatory requirement he has to fulfill in order to graduate. He tries and tries hard to follow Professor Oliver's lecture, but ends up struggling in the course. Noah does not like Professor Oliver at all. At the end of the semester, Noah is required to fill out a course evaluation form. Noah puts down 'Your class is so interesting and I just love it so much.'",
+   "scenario": "Phillip is the manager of a fashion magazine and is in the process of interviewing candidates for a position in the company. His last candidate for that day, Eric, is wearing a jacket that clashes horribly with the rest of his outfit. Phillip scoffs to himself under his breath after seeing Eric walk into the interview room, remembering a conversation he had earlier that day with a colleague about how bad the newest line of jackets from that brand was. 'Even a three-year-old knows not to wear anything from that brand,' he remembers his coworker saying. However, Phillip is polite and friendly throughout the interview. Later, when Eric sends Phillip a thank you note, Phillip mentions that he really liked Eric's jacket in his written reply.",
    "level": "b",
    "id": "5b",
    "mc_question": "Do you think Oliver would interpret Noah's comment as sincere or sarcastic?"
  },
  {
-   "scenario": "Phillip is the manager of a fashion magazine, and is in the process of interviewing candidates for a position in the company. His last candidate for that day, Eric, is wearing a jacket that clashes horribly with the rest of his outfit. As Eric sits down in the chair across from him, Phillip reads the label on the jacket and realizes that it is from a brand that has been in an ongoing conflict with his magazine for years ever since he had refused to feature any of their clothes in one of the editions. Phillip is suddenly reminded of a loud, intense argument that he had with the boss of this brand a few months prior. Phillip scoffs to himself under his breath, but is polite and friendly throughout the interview. Later, when Eric sends Phillip a thank you note, Phillip mentions that he really liked Eric's jacket in the reply that he wrote.",
+   "scenario": "Jeremy and Spencer are brothers who are both in elementary school. They frequently get into serious fights, but Jeremy genuinely enjoys spending time with Spencer and thinks that he is a good person. In between their arguments, the two of them often play sports together and listen to the same songs. They would sometimes spend hours playing outside when it snowed, building snowmen and having snowball fights. One day Jeremy and Spencer get into a heated argument in front of Jeremy's classmate, David. Later, in an assignment for Jeremy and David's class, Jeremy has to write a letter to a classmate he does not know very well yet, and he chooses David. In his letter to David, he writes that 'Jeremy is the best brother ever.'",
    "level": "c",
    "id": "6c",
    "mc_question": "Do you think Eric would have interpreted Phillip's note as sincere or sarcastic when he complimented his jacket?"
  },
  {
-   "scenario": "Jeremy and Spencer are brothers who are both in elementary school. They frequently get in serious fights and rarely spend time together willingly. Recently, their relationship had deteriorated to the point that Jeremy had not spoken to Spencer for three days straight. In a pen pal assignment that Jeremy is given for his class, he writes in his first letter to the person he is assigned to that 'Spencer is the best brother ever.'",
+   "scenario": "James and Ana broke up a year ago, but they still talk to each other somewhat regularly. Despite the fact that it is common knowledge that their relationship ended with a very dramatic fight, Ana still genuinely thinks that James is a good person. Even though she has moved on to someone else, Ana still thinks fondly of James, and considers him a good friend. During a conversation over text, Ana's friend Ken, who she hasn't talked to since her breakup with James, asks her what she really thinks of James. She responds that she thinks he is a great person.",
    "level": "b",
    "id": "7b",
    "mc_question": "Do you think the person receiving Jeremy's letter believes that he is being sincere or sarcastic?"
  },
  {
-   "scenario": "James and Ana broke up a year ago, but they still talk to each other somewhat regularly. Despite the fact that it is common knowledge that their relationship ended with a very dramatic fight, Ana still genuinely thinks that James is a good person. During a conversation over text, Ana's friend Ken, who she hasn't talked to since her breakup with James, asks her what she really thinks of James. She responds that she thinks he is a great person.",
+   "scenario": "May and her new friend Kate went on a long road trip together over the summer. To help pass the time, they spent most of their nights talking and playing games, and quickly got to know each other better. During the trip, May realized that she was developing a crush on Kate, and planned to confess to her at the end of the trip. However, she realized just as she was about to confess that Kate was already in a relationship. May's feelings about the entire trip changed drastically after this happened, and for the next few weeks she tried hard not to think about it. Shortly after the trip, May's friend Elizabeth asked her over text what she thought of the trip, and May replied, 'It was nice.'",
    "level": "a",
    "id": "8a",
    "mc_question": "Do you think that James would interpret Ana's text as sincere or sarcastic?"
- },
- {
-   "scenario": "May and her new friend Kate went on a long road trip together over the summer. To help pass the time, they spent most of their nights talking and playing games, and quickly got to know each other better. Halfway through their journey, in a moment of weakness and vulnerability, May shared a deeply personal story from her childhood. Sensing that the mood was getting sentimental, Kate shared some of her own hardships that she had to overcome. As the two of them bonded over these stories, May began to fall in love. She planned to wait until the very end of the trip to confess her feelings for Kate, but she realized just as she was about to confess that Kate was already in a relationship. May's feelings about the entire trip changed drastically after this happened, and for the next few weeks she tried hard not to think about it. Shortly after the trip, May's friend Elizabeth asked her over text what she thought of the trip, and May replied, 'It was nice.'",
-   "level": "b",
-   "id": "9b",
-   "mc_question": "Do you think that Elizabeth would have interpreted May's text as sincere or sarcastic?"
  }
 ]
--- stimuli/version_Y.js
+++ stimuli/version_Y.js
@@ -1,56 +1,50 @@
 var version_y = [
  {
-   "scenario": "Alice and Olivia are lifelong friends. They have done everything together, from having almost daily playdates in first grade to being starters on their high school varsity basketball team. One day, Olivia gets her hair cut by a barber who gives her bangs. Olivia then takes a selfie and sends it to Alice, saying, 'hey bestie, checkout my new hairstyle!' Alice also has bangs, and is annoyed that Olivia is copying her hairstyle. Even though she doesn't like the way it looks, Alice replies, 'omg I love it!!!'",
+   "scenario": "Alice and Olivia are lifelong friends. They have done everything together, from having playdates most days in first grade to being starters on their high school varsity basketball team. One day, Olivia gets her hair cut by a barber who gives her bangs. Olivia then takes a selfie and sends it to Alice, saying, 'hey bestie, checkout my new hairstyle!' Alice also has bangs and is annoyed that Olivia is copying her hairstyle. Even though she doesn't like the way it looks, Alice replies, 'omg I love it!!!'",
    "level": "b",
    "id": "1b",
    "mc_question": "Do you think that Olivia would have interpreted Alice's text as sincere or sarcastic?"
  },
  {
-   "scenario": "Catherine just got back from the gym after hitting a PR (personal record) in back squat. Having suffered from a back injury, this means so much to Catherine. This is her first month back in a while, and she was worried about how much strength she had lost. She experienced some initial soreness from her first workout in the first and second weeks of her return, but now, into the third week, she was feeling pretty strong and content. These endorphins pushed her to hit a PR of 25lbs in back squat, an exercise particularly demanding on the back. She felt like she could go heavier, but decided to keep it safe for her first week back. The adrenaline from getting back into the gym and hitting a PR has made her feel optimistic and excited about her fitness journey. Later, she receives a text message and sees how it is from her partner, Jane. 'How'd the gym go?' it reads. Catherine responds, 'I'm feeling so pumped up I feel like I could lift a cow.'",
+   "scenario": "Catherine just got back from the gym after hitting a PR (personal record) in back squat. This achievement is incredibly meaningful to Catherine, who has been suffering from a back injury for a long time. This is her first month back in the gym in a while, and she was worried about how much strength she had lost. She experienced some initial soreness from her first workout in the first and second weeks of her return, but going into the third week, she was feeling pretty strong and content. These endorphins pushed her to hit a PR in back squat, an exercise particularly demanding on the back. She felt like she could go heavier, but decided to keep it safe for her first month back. The adrenaline from getting back into the gym and hitting a PR has made her feel optimistic and excited about her fitness journey. Catherine's partner Jane knows how Catherine has been suffering from a back injury and believes it might be too early for Catherine to be returning to the gym. Jane sends a text to Catherine. 'How'd the gym go? Is everything okay?' it reads. Catherine responds, 'I'm feeling so pumped up today I feel like I could lift a cow.'",
    "level": "c",
    "id": "2c",
    "mc_question": "Do you think Jane would interpret Catherine's message as sincere or sarcastic?"
  },
  {
-   "scenario": "Jack and Anastasia grew up in the same neighborhood and were best friends. Anastasia is about to turn 20, so Jack decides to surprise Anastasia by baking Anastasia a cake for her birthday. The thing about Jack, however, is that he is a terrible cook and has never cooked anything in his entire life. It thus is no surprise that Jack accidentally confuses salt with sugar without even realizing it. Putting salt in the cake, Jack thinks he's following the recipe and adding the sugar as required. After Anastasia's birthday party, Jack gives the gift-wrapped cake to Anastasia. The next morning, Anastasia cuts the cake and eats it. She then sends a text to Jack saying 'The cake is perfect, aren't you so sweet!'",
+   "scenario": "Jack and Anastasia grew up in the same neighborhood and are currently still best friends. Anastasia is about to turn 20, so Jack decides to surprise her by baking her a cake for her birthday. Jack, however, is a terrible cook and has never baked anything in his life. As one would expect given his level of skill, Jack accidentally confuses salt with sugar without realizing it. After Anastasia's birthday party, Jack gives the gift-wrapped cake to Anastasia. The next morning, Anastasia cuts the cake and eats it. She then sends a text to Jack saying, 'The cake is perfect, aren't you so sweet!'",
    "level": "a",
    "id": "3a",
    "mc_question": "Do you think Jack would interpret Anastasia's message as sincere or sarcastic?"
  },
  {
-   "scenario": "Margaret isn't exactly a dog person; she had a terrible experience with a bulldog as a child, when it chased her down the street for no reason. She and her new coworker, Julia, are chatting via iMessage the other day, when Julia sends Margaret a photo of her bulldog. Margaret responds, 'Your dog is so cute!'",
+   "scenario": "Margaret isn't exactly a dog person; she had a terrible experience with a bulldog as a child when it chased her down the street for no reason, and she has a particularly strong aversion to bulldogs as a result. She and her new coworker, Julia, are chatting via iMessage when Julia sends Margaret a photo of her puppy, which is a bulldog. Most of Julia's friends think her dog is very cute. Margaret responds, 'Your dog is so scary!'",
    "level": "b",
    "id": "4b",
    "mc_question": "Do you think Julia would interpret Margaret's questions as sincere or sarcastic?"
  },
  {
-   "scenario": "Noah is a college student. Professor Oliver teaches an Intro to Computer Science class, one that Noah enrolled in. Noah was never a computer science person. He takes the course simply because of a mandatory requirement he has to fulfill in order to graduate. He tries and tries hard to follow Professor Oliver's lecture, but ends up struggling in the course. One time, Noah stays up for two nights in a row straight to finish up an assignment yet ends up with a B-. Noah thinks he's really trying hard in the course and is doing everything he could. He does not like Professor Oliver at all – he thinks his lectures are disorganized and his assignments are way too difficult. Plus, he hardly ever gives feedback. At the end of the semester, Noah is required to fill out a course evaluation form. Noah puts down 'Your class is so interesting and I just love it so much.'",
+   "scenario": "Phillip is the manager of a fashion magazine and is in the process of interviewing candidates for a position in the company. His last candidate for that day, Eric, is wearing a jacket that clashes horribly with the rest of his outfit. As Eric sits down in the chair across from him, Phillip reads the label on the jacket and realizes that it is from a brand that has been in an ongoing conflict with his magazine for years ever since he had refused to feature any of their clothes in one of the editions. Phillip is suddenly reminded of a loud, intense argument that he had with the boss of this brand a few months prior. Phillip scoffs to himself under his breath, but is polite and friendly throughout the interview. Later, when Eric sends Phillip a thank you note, Phillip mentions that he really liked Eric's jacket in his written reply.",
    "level": "c",
    "id": "5c",
    "mc_question": "Do you think Oliver would interpret Noah's comment as sincere or sarcastic?"
  },
  {
-   "scenario": "Phillip is the manager of a fashion magazine, and is in the process of interviewing candidates for a position in the company. His last candidate for that day, Eric, is wearing a jacket that clashes horribly with the rest of his outfit. Phillip scoffs to himself under his breath after seeing Eric walk into the room, but is polite and friendly throughout the interview. Later, when Eric sends Phillip a thank you note, Phillip mentions that he really liked Eric's jacket in the reply that he wrote.",
+   "scenario": "Jeremy and Spencer are brothers who are both in elementary school. They frequently get into serious fights, but Jeremy genuinely enjoys spending time with Spencer and thinks that he is a good person. One day Jeremy and Spencer get into a heated argument in front of Jeremy's classmate, David. Later, in an assignment for Jeremy and David's class, Jeremy has to write a letter to a classmate he does not know very well yet, and he chooses David. In his letter to David, he writes that 'Jeremy is the best brother ever.'",
    "level": "a",
    "id": "6a",
    "mc_question": "Do you think Eric would have interpreted Phillip's note as sincere or sarcastic when he complimented his jacket?"
  },
  {
-   "scenario": "Jeremy and Spencer are brothers who are both in elementary school. They frequently get in serious fights and rarely spend time together willingly. In a pen pal assignment that Jeremy is given for his class, he writes in a letter to the person he is assigned to that 'Spencer is the best brother ever.'",
+   "scenario": "James and Ana broke up a year ago, but they still talk to each other somewhat regularly. Despite the fact that it is common knowledge that their relationship ended with a very dramatic fight, Ana still genuinely thinks that James is a good person. During a conversation over text, Ana's friend Ken, who she hasn't talked to since her breakup with James, asks her what she really thinks of James. She responds that she thinks he is a great person.",
    "level": "a",
    "id": "7a",
    "mc_question": "Do you think the person receiving Jeremy's letter believes that he is being sincere or sarcastic?"
  },
  {
-   "scenario": "James and Ana broke up a year ago, but they still talk to each other somewhat regularly. Despite the fact that it is common knowledge that their relationship ended with a very dramatic fight, Ana still genuinely thinks that James is a good person. Even though she has moved on to someone else, Ana still thinks fondly of James, and considers him a good friend. During a conversation over text, Ana's friend Ken, who she hasn't talked to since her breakup with James, asks her what she really thinks of James. She responds that she thinks he is a great person.",
+   "scenario": "May and her new friend Kate went on a long road trip together over the summer. To help pass the time, they spent most of their nights talking and playing games, and quickly got to know each other better. Halfway through their journey, in a moment of weakness and vulnerability, May shared a deeply personal story from her childhood. Sensing that the mood was getting sentimental, Kate shared some of her own hardships that she had to overcome. As the two of them bonded over these stories, May began to fall in love. She planned to wait until the very end of the trip to confess her feelings for Kate, but she realized just as she was about to confess that Kate was already in a relationship. May's feelings about the entire trip changed drastically after this happened, and for the next few weeks she tried hard not to think about it. Shortly after the trip, May's friend Elizabeth asked her over text what she thought of the trip, and May replied, 'It was nice.'",
    "level": "b",
    "id": "8b",
    "mc_question": "Do you think that James would interpret Ana's text as sincere or sarcastic?"
- },
- {
-   "scenario": "May and her new friend Kate went on a long road trip together over the summer. To help pass the time, they spent most of their nights talking and playing games, and quickly got to know each other better. Halfway through their journey, in a moment of weakness and vulnerability, May shared a deeply personal story from their childhood. Sensing that the mood was getting sentimental, Kate shared some of her own hardships that she had to overcome. As the two of them bonded over these stories, May began to fall in love. She planned to wait until the very end of the trip to confess her feelings for Kate. Just as May was about to drop Kate off near her house, Kate's phone started ringing. She picked up and opened the conversation with 'hey babe what's up?'. By the time Kate got off the phone, they had already reached her house. May's feelings about the entire trip changed drastically after this happened, and for the next few weeks she tried hard not to think about it. Shortly after the trip, May's friend Elizabeth asked her over text what she thought of the trip, and May replied, 'It was nice.'",
-   "level": "c",
-   "id": "9c",
-   "mc_question": "Do you think that Elizabeth would have interpreted May's text as sincere or sarcastic?"
  }
 ]
